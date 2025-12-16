3d5ef79e532799d6cf554ef7280a5c9be2ce5264
Tanya-g99 <tanya-g99@ya.ru>
2025-12-16 11:44:26 +0300

    add fetching data on mounted

--- front/src/App.vue
+++ front/src/App.vue
@@ -8,6 +8,20 @@
 
 <script lang="ts" setup>
 import Navbar from '@/components/Navbar.vue';
+import { usePriceTagStore } from '@/stores/priceTagStore';
+import { useProductStore } from '@/stores/productStore';
+import { useTemplateStore } from '@/stores/templateStore';
+import { onMounted } from 'vue';
+
+const productStore = useProductStore();
+const priceTagStore = usePriceTagStore();
+const templateStore = useTemplateStore();
+
+onMounted(async () => {
+  await productStore.fetchProducts();
+  await priceTagStore.fetchPriceTags();
+  await templateStore.fetchPriceTagTemplates();
+});
 </script>
 
 <style scoped>
